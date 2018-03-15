b41c016d24feeb9ad388b42e22d1a31b50a1ab55
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-15 11:07:14 +0100

    Add GET method to Service

--- VelibGateway-Service/Service1.cs
+++ VelibGateway-Service/Service1.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace VelibGateway_Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    
     public class Service1 : IService1
     {
+        [WebInvoke( Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "data/{value}")]
         public string GetData(string value)
         {
             return string.Format("You entered: "+value);
