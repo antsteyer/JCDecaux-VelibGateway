75a9c2fb311d79b277116ff572826583117e3369
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-15 11:26:22 +0100

    Add reference to VelibGateway-Service in VelibGateway-Client

--- VelibGateway-Service/IService1.cs
+++ VelibGateway-Service/IService1.cs
@@ -7,13 +7,11 @@ using System.Text;
 
 namespace VelibGateway_Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
         string GetData(string value);
-
-        // TODO: Add your service operations here
+        
     }
 }
