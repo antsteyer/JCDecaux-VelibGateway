3b3a4e0ed4936da4834adcf4d7a95d4120a47b34
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-15 10:59:21 +0100

    Change basicHttpBinding to webHttpBinding

--- VelibGateway-Service/Service1.cs
+++ VelibGateway-Service/Service1.cs
@@ -10,22 +10,9 @@ namespace VelibGateway_Service
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string GetData(string value)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return string.Format("You entered: "+value);
         }
     }
 }
