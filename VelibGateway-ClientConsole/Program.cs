880290287d60606baf8b2d14cf4bb988c0270f69
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-22 10:46:59 +0100

    Add simple Console client

---
+++ VelibGateway-ClientConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelibGateway_ClientConsole.VelibServiceReference;
+
+namespace VelibGateway_ClientConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ServiceClient client = new ServiceClient();
+
+      Console.WriteLine(client.showCoucou());
+      Console.ReadLine();
+    }
+  }
+}
