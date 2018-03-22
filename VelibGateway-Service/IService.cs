7f01bfc15e41a17e87f4357f3ce73170b452dfab
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-22 10:46:36 +0100

    Renamed Service classes and configured App.config

---
+++ VelibGateway-Service/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace VelibGateway_Service
+{
+  [ServiceContract]
+  public interface IService
+  {
+    [OperationContract]
+    string showCoucou();
+  }
+}
---
+++ VelibGateway-Service/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VelibGateway_Service
+{
+  public class Service : IService
+  {
+    public string showCoucou()
+    {
+      return "Coucou";
+    }
+  }
+}
