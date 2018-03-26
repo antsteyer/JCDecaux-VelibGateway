f408fc7b8fe07e2f769322b0ba0b6d3f94819200
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-26 21:05:59 +0200

    Created models for contracts, stations and positions objects of the API

---
+++ VelibGateway-Service/model/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VelibGateway_Service.model
+{
+  class Contract
+  {
+    public String name { get; set; }
+    public List<String> cities { get; set; }
+    public String commercial_name { get; set; }
+    public String country_code { get; set; }
+  }
+}
---
+++ VelibGateway-Service/model/Station.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VelibGateway_Service.model
+{
+  class Station
+  {
+    public int number { get; set; }
+    public String name { get; set; }
+    public String address { get; set; }
+    public Position position { get; set; }
+    public Boolean banking { get; set; }
+    public Boolean bonus { get; set; }
+    public String status { get; set; }
+    public String contract_name { get; set; }
+    public int bike_stands { get; set; }
+    public int available_bike_stands { get; set; }
+    public int available_bikes { get; set; }
+    public long last_update { get; set; }
+  }
+}
