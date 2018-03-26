4510419faedf7e4ea51a52e5a2b8a96a9d2534e6
antsteyer <antoine.steyer@etu.unice.fr>
2018-03-26 20:06:55 +0200

    Class to connect to the Velib API with REST

---
+++ VelibGateway-Service/VelibWeb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VelibGateway_Service
+{
+  class VelibWeb
+  {
+    private String APIkey;
+    private static String baseVelibURL = "https://api.jcdecaux.com/vls/v1/";
+
+    public VelibWeb(String APIkey)
+    {
+      this.APIkey = APIkey;
+    }
+
+    public String ConnectToAPI(String requestParams)
+    {
+      WebRequest request = WebRequest.Create(baseVelibURL + requestParams + "&apiKey=" + APIkey);
+      return getResponseToString(request.GetResponse());
+    }
+
+    private String getResponseToString(WebResponse response)
+    {
+      Stream dataStream = response.GetResponseStream();
+      StreamReader reader = new StreamReader(dataStream);
+      String responseFromServer = reader.ReadToEnd();
+      reader.Close();
+      response.Close();
+      return responseFromServer;
+    }
+
+  }
+}
